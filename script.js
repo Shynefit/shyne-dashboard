06820f74645fb90cb9cd6a9047dc515381a6b2c3
Shynefit <shyne58franco@gmail.com>
2026-07-24 14:13:27 +0800

    Add weight saving functionality to script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
 function sayHello() {
     alert("Hello Bes! 💙 Welcome to Shyne OS!");
 }
+
+function saveWeight() {
+
+    let weight = document.getElementById("weightInput").value;
+
+    document.getElementById("weightDisplay").innerText = weight + " kg";
+
+    localStorage.setItem("weight", weight);
+
+}
+
+window.onload = function () {
+
+    let savedWeight = localStorage.getItem("weight");
+
+    if (savedWeight) {
+        document.getElementById("weightDisplay").innerText =
+            savedWeight + " kg";
+    }
+
+}
