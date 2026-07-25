9b49a0d46611d2bda33fe8644354875dc75af14b
Shynefit <shyne58franco@gmail.com>
2026-07-25 11:09:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,46 +3,40 @@ function sayHello() {
 }
 
 function saveWeight() {
-
     let weight = document.getElementById("weightInput").value;
 
     document.getElementById("weightDisplay").innerText = weight + " kg";
-
     localStorage.setItem("weight", weight);
 
     document.getElementById("weightInput").value = "";
 
     let toast = document.getElementById("toast");
-
-toast.classList.add("show");
-
-setTimeout(function () {
-    toast.classList.remove("show");
-}, 2000);
-}
-function saveGoal() {
-
-    let goal = document.getElementById("goalInput").value;
-
-    document.getElementById("goalDisplay").innerText = goal + " kg";
-
-    localStorage.setItem("goal", goal);
-
-    document.getElementById("goalInput").value = "";
-
-    let toast = document.getElementById("toast");
-
-    toast.innerText = "🎯 Goal saved!";
-
+    toast.innerText = "✅ Weight saved!";
     toast.classList.add("show");
 
     setTimeout(function () {
         toast.classList.remove("show");
     }, 2000);
 }
-function addWorkout() {
-    alert("addWorkout is running!");
 
+function saveGoal() {
+    let goal = document.getElementById("goalInput").value;
+
+    document.getElementById("goalDisplay").innerText = goal + " kg";
+    localStorage.setItem("goal", goal);
+
+    document.getElementById("goalInput").value = "";
+
+    let toast = document.getElementById("toast");
+    toast.innerText = "🎯 Goal saved!";
+    toast.classList.add("show");
+
+    setTimeout(function () {
+        toast.classList.remove("show");
+    }, 2000);
+}
+
+function addWorkout() {
     let streak = localStorage.getItem("streak");
 
     if (!streak) {
@@ -50,6 +44,7 @@ function addWorkout() {
     }
 
     streak = parseInt(streak) + 1;
+
     localStorage.setItem("streak", streak);
 
     document.getElementById("streakDisplay").innerText =
@@ -63,21 +58,25 @@ function addWorkout() {
         toast.classList.remove("show");
     }, 2000);
 }
+
 window.onload = function () {
 
     let savedWeight = localStorage.getItem("weight");
+
     if (savedWeight) {
         document.getElementById("weightDisplay").innerText =
             savedWeight + " kg";
     }
 
     let savedGoal = localStorage.getItem("goal");
+
     if (savedGoal) {
         document.getElementById("goalDisplay").innerText =
             savedGoal + " kg";
     }
 
     let savedStreak = localStorage.getItem("streak");
+
     if (savedStreak) {
         document.getElementById("streakDisplay").innerText =
             savedStreak + " Days";
